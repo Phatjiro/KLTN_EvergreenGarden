1254ad9decb28c25be3b869a5005e8fdd15d4ca2
s1mple-n <77015087+s1mple-n@users.noreply.github.com>
2023-11-08 00:49:44 +0700

    limit camera move

--- Assets/Scripts/GameLogic/CameraMovement.cs
+++ Assets/Scripts/GameLogic/CameraMovement.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
@@ -7,8 +8,10 @@ public class CameraMovement : MonoBehaviour
 
     public Vector3 offset = new Vector3 (0, 0, -10); // Positon of character in main screen
 
+    public Vector3 minValues, maxValue;
+
     // LateUpdate run after function Update run
-    private void LateUpdate()
+/*    private void LateUpdate()
     {
         if (target != null)
         {
@@ -16,5 +19,22 @@ public class CameraMovement : MonoBehaviour
             Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
             transform.position = smoothedPosition;
         }
+    }*/
+    private void FixedUpdate()
+    {
+        Follow();
+    }
+    void Follow()
+    {
+        Vector3 targetPosition = target.position + offset;
+        //Verify if the targetPosition is out of bound or not
+        //Limit it to the min and max values
+        Vector3 boundPosition = new Vector3(
+            Mathf.Clamp(targetPosition.x, minValues.x, maxValue.x),
+            Mathf.Clamp(targetPosition.y, minValues.y, maxValue.y),
+            Mathf.Clamp(targetPosition.z, minValues.z, maxValue.z));
+
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, boundPosition, smoothSpeed * Time.fixedDeltaTime);
+        transform.position = smoothPosition;
     }
 }
